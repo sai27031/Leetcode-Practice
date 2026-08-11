b0b66afff99e93ccfa9dadc6bc2f722a3c8820c8
Sai Sujan <129964894+sai27031@users.noreply.github.com>
2026-08-11 20:52:20 +0530

    Time: 7 ms (72.14%) | Memory: 19.5 MB (13.18%) - LeetSync

---
+++ 38-count-and-say/count-and-say.py
@@ -0,0 +1,17 @@
+class Solution:
+    def countAndSay(self, n: int) -> str:
+        result = "1"
+        for i in range(n - 1):
+            current = []
+            count = 1
+            for i in range(1, len(result)):
+                if result[i] == result[i - 1]:
+                    count += 1
+                else:
+                    current.append(str(count))
+                    current.append(result[i - 1])
+                    count = 1
+            current.append(str(count))
+            current.append(result[-1])
+            result = "".join(current)
+        return result 
