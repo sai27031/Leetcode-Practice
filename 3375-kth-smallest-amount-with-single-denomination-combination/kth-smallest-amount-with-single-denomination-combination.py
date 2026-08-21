91b42eeb4b65f74701d4b89c84063b8b8bd5a307
Sai Sujan <129964894+sai27031@users.noreply.github.com>
2026-08-21 20:45:04 +0530

    Time: 151 ms (43.90%) | Memory: 21.1 MB (10.98%) - LeetSync

--- 3375-kth-smallest-amount-with-single-denomination-combination/kth-smallest-amount-with-single-denomination-combination.py
+++ 3375-kth-smallest-amount-with-single-denomination-combination/kth-smallest-amount-with-single-denomination-combination.py
@@ -1,46 +1,36 @@
 class Solution:
     def findKthSmallest(self, coins: List[int], k: int) -> int:
+        from typing import List
         from math import gcd
-
         n = len(coins)
-
-        def lcm(a, b):
-            return a // gcd(a, b) * b
-
+        subsets = []
+        for mask in range(1, 1 << n):
+            bit = mask & -mask
+            i = bit.bit_length() - 1
+            prev = mask ^ bit
+            if prev == 0:
+                L = coins[i]
+            else:
+                prev_lcm = subsets[prev - 1][0]
+                L = prev_lcm // gcd(prev_lcm, coins[i]) * coins[i]
+            if L <= min(coins) * k:
+                sign = 1 if mask.bit_count() % 2 else -1
+                subsets.append((L, sign))
+            else:
+                subsets.append((L, 0))
         def count(x):
             total = 0
-
-            for mask in range(1, 1 << n):
-                L = 1
-                bits = 0
-
-                for i in range(n):
-                    if mask & (1 << i):
-                        bits += 1
-                        L = lcm(L, coins[i])
-
-                        if L > x:
-                            break
-
+            for L, sign in subsets:
                 if L > x:
                     continue
-
-                if bits % 2 == 1:
-                    total += x // L
-                else:
-                    total -= x // L
-
+                total += sign * (x // L)
             return total
-
         left = 1
         right = min(coins) * k
-
         while left < right:
             mid = (left + right) // 2
-
             if count(mid) >= k:
                 right = mid
             else:
                 left = mid + 1
-
         return left
